0e227b5b6261950e5c6c6f618fca64cc67be4b7c
TuanLT <le.thanh.tuan@quantic.com.vn>
2023-02-25 08:34:05 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Test from './components/common/Header/aaa/bbb/ccc/Test'
+import Header from './components/common/Header/Header'
+
+function App() {
+    return (
+        <>
+            {/* <Header /> */}
+            <Test />
+        </>
+    )
+}
+
+export default App
---
+++ src/components/common/Header/aaa/bbb/ccc/Test.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../../../Header'
+
+type Props = {}
+
+const Test = (props: Props) => {
+    return (
+        <div>
+            <Header />
+        </div>
+    )
+}
+
+export default Test
